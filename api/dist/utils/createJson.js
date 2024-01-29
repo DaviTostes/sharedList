d0a2dab65380f53af76fdca66043962d377baa25
Davi Tostes <60412948+DaviTostes@users.noreply.github.com>
2024-01-29 17:13:55 -0300

    Delete api/dist directory

--- api/dist/utils/createJson.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function createMessage(message, data) {
-    return { message, data };
-}
-exports.default = createMessage;
-//# sourceMappingURL=createJson.js.map
